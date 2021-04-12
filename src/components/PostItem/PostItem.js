5d9362abe57be2d3f72bac2e1727423b16f08a46
Hoaibacez <75563324+Hoaibacez@users.noreply.github.com>
2021-04-12 16:41:02 +0700

    post

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -1,65 +1,84 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faComment, faShare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faComment, faShare } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import config from "../../config";
 
-import video01 from '../../assets/videos/video_01.mp4'
-import styles from './PostItem.module.scss'
+import video01 from "../../assets/videos/phuongly.mp4";
+import styles from "./PostItem.module.scss";
 
-const defaultFn = () => {}
+const defaultFn = () => {};
 
 function PostItem({
-    avatar = '',
-    nickname = '',
-    username = '',
-    publishedAt = '',
-    postContent = '',
-    likeCount = 0,
-    commentCount = 0,
-    shareCount = 0,
-    onLike = defaultFn,
-    onComment = defaultFn,
-    onShare = defaultFn,
-    onShowDetail = defaultFn
+  avatar = "",
+  nickname = "",
+  username = "",
+  publishedAt = "",
+  postContent = "",
+  hasTag = "",
+  likeCount = 0,
+  commentCount = 0,
+  shareCount = 0,
+  onLike = defaultFn,
+  onComment = defaultFn,
+  onShare = defaultFn,
+  onShowDetail = defaultFn
 }) {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.header}>
-                <img src={avatar} className={styles.avatar} alt={nickname} />
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <Link to={`${config.routes.home}@${username}`}>
+          <img src={avatar} className={styles.avatar} alt={nickname} />
+        </Link>
 
-                <div className={styles.info}>
-                    <div className={styles.headingBox}>
-                        <h3 className={styles.nickname}>{nickname}</h3>
-                        <p className={styles.username}>{username}</p>
-                        <span> · </span>
-                        <p className={styles.time}>{publishedAt}</p>
-                    </div>
-                    <div className={styles.postContent}>
-                        {postContent}
-                    </div>
-                </div>
+        <div className={styles.info}>
+          <Link to={`${config.routes.home}@${username}`}>
+            <div className={styles.headingBox}>
+              <h3 className={styles.nickname}>{nickname}</h3>
+              <p className={styles.username}>{username}</p>
+              <span> · </span>
+              <p className={styles.time}>{publishedAt}</p>
             </div>
-            
-            <div className={styles.content}>
-                <video src={video01} className={styles.video} onClick={onShowDetail} />
+          </Link>
 
-                <div className={styles.actionBox}>
-                    <button className={styles.actionBtn} onClick={onLike}>
-                        <FontAwesomeIcon icon={faHeart} className={styles.actionIcon} />
-                    </button>
-                    <span className={styles.reactionCount}>{likeCount}</span>
-                    
-                    <button className={styles.actionBtn} onClick={onComment}>
-                        <FontAwesomeIcon icon={faComment} className={styles.actionIcon} />
-                    </button>
-                    <span className={styles.reactionCount}>{commentCount}</span>
-                    
-                    <button className={styles.actionBtn} onClick={onShare}>
-                        <FontAwesomeIcon icon={faShare} className={styles.actionIcon} />
-                    </button>
-                    <span className={styles.reactionCount}>{shareCount}</span>
-                </div>
-            </div>
+          <div className={styles.postContent}>
+            {postContent}
+            <Link
+              to={`${config.routes.home}@${username}`}
+              className={styles.hasTag}
+            >
+              {hasTag}
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className={styles.content}>
+        <video
+          src={video01}
+          className={styles.video}
+          onClick={onShowDetail}
+          controls
+        />
+
+        <div className={styles.actionBox}>
+          <button className={styles.actionBtn} onClick={onLike}>
+            <FontAwesomeIcon icon={faHeart} className={styles.actionIcon} />
+          </button>
+          <span className={styles.reactionCount}>{likeCount}</span>
+
+          <button className={styles.actionBtn} onClick={onComment}>
+            <FontAwesomeIcon icon={faComment} className={styles.actionIcon} />
+          </button>
+          <span className={styles.reactionCount}>{commentCount}</span>
+
+          <button className={styles.actionBtn} onClick={onShare}>
+            <FontAwesomeIcon icon={faShare} className={styles.actionIcon} />
+          </button>
+          <span className={styles.reactionCount}>{shareCount}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default PostItem
+export default PostItem;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,56 +1,61 @@
-import { useHistory } from 'react-router-dom'
-import { Row, Column } from '@mycv/mycv-grid'
+import { useHistory } from "react-router-dom";
+import { Row, Column } from "@mycv/mycv-grid";
 
-import MainSidebar from '../../components/MainSidebar'
-import PostItem from '../../components/PostItem'
+import avatar from "../../assets/img/phuong_ly.jpeg";
+import MainSidebar from "../../components/MainSidebar";
+import PostItem from "../../components/PostItem";
 
 function Home() {
-    const history = useHistory()
+  const history = useHistory();
 
-    const handleLogin = () => {
-        alert('XU LY LOGIN')
+  const handleLogin = () => {
+    alert("XU LY LOGIN");
+  };
+
+  const posts = [
+    {
+      id: 1,
+      avatar: avatar,
+      nickname: "phuonglytran",
+      username: "Phương Ly Trần",
+      publishedAt: "10 phút",
+      postContent: "Baby , leave the door open 😆",
+      hasTag: "#phuongly #leavethedooropen",
+      likeCount: "",
+      commentCount: "",
+      shareCount: ""
     }
+  ];
 
-    const posts = [
-        {
-            id: 1,
-            avatar: 'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/14af51e8ed6fdebd007a36fbfb8d3f0b.jpeg?x-expires=1617976800&x-signature=GKia80uBiWk7f0cFsieyFx%2Fi9jg%3D',
-            nickname: 'orelregus',
-            username: 'Billiards style66',
-            publishedAt: '10 phút',
-            postContent: '#xuhuongtiktok #xuhuong mọi người lưu ý nha',
-            likeCount: '',
-            commentCount: '',
-            shareCount: '',
-        }
-    ]
-
-    return (
-        <Row>
-            <Column size={0} sizeTablet={4} sizeDesktop={3}>
-                <MainSidebar onLogin={handleLogin} />
-            </Column>
-            <Column size={12} sizeTablet={8} sizeDesktop={9}>
-                {posts.map(post => (
-                    <PostItem
-                        key={post}
-                        avatar={post.avatar}
-                        nickname={post.nickname}
-                        username={post.username}
-                        publishedAt={post.publishedAt}
-                        postContent={post.postContent}
-                        likeCount={post.likeCount}
-                        commentCount={post.commentCount}
-                        shareCount={post.shareCount}
-                        onLike={post.onLike}
-                        onComment={post.onComment}
-                        onShare={post.onShare}
-                        onShowDetail={() => history.push('/@baoboii_002/video/6948366460460764418')}
-                    />
-                ))}
-            </Column>
-        </Row>
-    )
+  return (
+    <Row>
+      <Column size={0} sizeTablet={4} sizeDesktop={3}>
+        <MainSidebar onLogin={handleLogin} />
+      </Column>
+      <Column size={12} sizeTablet={8} sizeDesktop={9}>
+        {posts.map((post) => (
+          <PostItem
+            key={post}
+            avatar={post.avatar}
+            nickname={post.nickname}
+            username={post.username}
+            publishedAt={post.publishedAt}
+            postContent={post.postContent}
+            hasTag={post.hasTag}
+            likeCount={post.likeCount}
+            commentCount={post.commentCount}
+            shareCount={post.shareCount}
+            onLike={post.onLike}
+            onComment={post.onComment}
+            onShare={post.onShare}
+            onShowDetail={() =>
+              history.push("/@baoboii_002/video/6948366460460764418")
+            }
+          />
+        ))}
+      </Column>
+    </Row>
+  );
 }
 
-export default Home
+export default Home;
