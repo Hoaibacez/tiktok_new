a90cedc23f19453bc176258da21980de690fb4ae
Hoaibacez <75563324+Hoaibacez@users.noreply.github.com>
2021-04-20 13:29:51 +0700

    color scroll

--- src/components/MainSidebar/MainSidebar.js
+++ src/components/MainSidebar/MainSidebar.js
@@ -1,34 +1,32 @@
-import { OverlayScrollbarsComponent } from 'overlayscrollbars-react'
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 
-import TopSidebar from './TopSidebar'
-import SuggestAccount from './SuggestAccount'
-import Footer from './Footer'
-import styles from './MainSidebar.module.scss'
-// import phuonglytran from '../../assets/img/phuong_ly.jpeg'
-// import my_vanh from '../../assets/img/my_vanh.jpeg'
-// import ameemee from '../../assets/img/ameemee.jpeg'
-// import Min_Min from '../../assets/img/minminmin0712.jpeg'
+import TopSidebar from "./TopSidebar";
+import SuggestAccount from "./SuggestAccount";
+import Footer from "./Footer";
+import styles from "./MainSidebar.module.scss";
+import "./scroll.scss";
 
 function MainSidebar({ onLogin, sugestAcount }) {
-    return (
-        <OverlayScrollbarsComponent
-            className={styles.wrapper}
-            options={{ 
-                scrollbars: { 
-                    autoHide: 'leave',
-                    autoHideDelay: 100
-                } 
-            }} 
-        >
-            <TopSidebar onLogin={onLogin} />
+  return (
+    <OverlayScrollbarsComponent
+      className={styles.wrapper}
+      style={{
+        paddingRight: "12px"
+      }}
+      options={{
+        scrollbars: {
+          autoHide: "leave",
+          autoHideDelay: 50
+        }
+      }}
+    >
+      <TopSidebar onLogin={onLogin} />
 
-            <SuggestAccount
-                data={sugestAcount}
-            />
+      <SuggestAccount data={sugestAcount} />
 
-            <Footer />
-        </OverlayScrollbarsComponent>   
-    )
+      <Footer />
+    </OverlayScrollbarsComponent>
+  );
 }
 
-export default MainSidebar
+export default MainSidebar;
